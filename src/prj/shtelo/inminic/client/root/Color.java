f1883782df25544d04644560dd9506591a4d2341
Sch <junhg0211@gmail.com>
2018-12-01 20:27:33 +0900

    Alpha_I 9.1

--- src/prj/shtelo/inminic/client/root/Color.java
+++ src/prj/shtelo/inminic/client/root/Color.java
@@ -1,7 +1,7 @@
 package prj.shtelo.inminic.client.root;
 
 public class Color {
-    public static java.awt.Color background = new java.awt.Color(222, 222, 222);
+    public static java.awt.Color background = new java.awt.Color(130, 189, 222);
 
     public static java.awt.Color text = new java.awt.Color(31, 33, 37);
 }
